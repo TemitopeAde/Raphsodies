e05108913ec19dd9cdf81a0ea85fbb3f54b87d2f
TemitopeAde <adesiyantope2014@gmail.com>
2025-03-05 12:45:38 +0100

    updates

--- src/app/(shop)/products/page.jsx
+++ src/app/(shop)/products/page.jsx
@@ -72,7 +72,7 @@ const Page = () => {
   };
 
   return (
-    <Suspense fallback={<div>Loading...</div>}>
+    <Suspense fallback={<div className="animate-spin rounded-full h-12 w-12 border-t-2 border-b-2 border-gray-900"></div>}>
       <section>
         <section className="relative h-screen w-screen">
           <div className="absolute inset-0">
